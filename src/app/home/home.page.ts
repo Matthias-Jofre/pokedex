6d12c901064e231941315a6fef85fdd6deddc95d
Matthias <matthias.jofre@gmail.com>
2022-05-15 18:32:33 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  offset = 0;
+  pokemon = [];
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
 
-  constructor() {}
+  constructor(private pokeService: PokemonService) {}
+
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  loadPokemon(loadMore = false, event?) {
+    if(loadMore) {
+      this.offset += 25;
+    }
+
+    this.pokeService.getPokemon(this.offset).subscribe(res => {
+      // console.log('result: ', res);
+        this.pokemon = [...this.pokemon, ...res];
+
+        if (event) {
+          event.target.complete();
+        }
+
+        if (this.offset == 125) {
+          this.infinite.disabled = true;
+        }
+    });
+  }
+
+  onSearchChange(e) {
+    // eslint-disable-next-line prefer-const
+    let value = e.detail.value;
+
+    // eslint-disable-next-line eqeqeq
+    if (value == '') {
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+
+    this.pokeService.findPokemon(value).subscribe(res => {
+      this.pokemon = [res];
+    }, err => {
+      this.pokemon = [];
+    });
+  }
 
 }
